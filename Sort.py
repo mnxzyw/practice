b5e36646fb36c7ea35cdbca107e68c43bce313ab
mnxzyw <mnxzyw@foxmail.com>
2018-08-18 20:50:11 +0800

    add bubble sort

--- Sort.py
+++ Sort.py
@@ -7,13 +7,25 @@
 
 
 #bubble sort
-def Bubble_sort():
+def Bubble_sort(la):
     print("Bubble_sort")
+    if(None==la):
+        return
+    for j in range(len(la)):
+        for i in range(len(la)-j-1):
+            if(la[i]>la[i+1]):
+                la[i],la[i+1]=la[i+1],la[i]
 
 
-def test():
-    Bubble_sort();
+def Print(arr):
+    for temp in arr:
+        print(temp)
 
+def test(arr):
+    Bubble_sort(arr);
+    Print(arr);
 
 if __name__=='__main__':
-    test();
+
+    array=[23,12,32,56,21,1,5,9];
+    test(array);
