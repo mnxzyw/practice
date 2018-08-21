c03939935481d4e9c7562e0c26e20bd435b8d54c
mnxzyw <mnxzyw@foxmail.com>
2018-08-21 18:50:41 +0800

    add quick sort

--- Sort.py
+++ Sort.py
@@ -7,6 +7,7 @@
 
 
 #bubble sort
+
 def Bubble_sort(la):
     print("Bubble_sort")
     if(None==la):
@@ -32,9 +33,45 @@ def Select_sort(la):
                 la[min],la[i]=la[i],la[min]
 
 
+#insert sort
+def Insert_sort(la):
+    for i in range(1,len(la)):
+        j=i-1
+        temp=la[i]
+        while((temp<la[j]) and (j>=0)):
+            la[j+1]=la[j]
+            j-=1
+        la[j+1]=temp
 
 
 
+#quick sort
+def f_sort(arr,nlow,nhigh):
+    temp=arr[nlow]
+    while(nlow<nhigh):
+        while(nlow<nhigh):
+            if(arr[nhigh]<temp):
+                arr[nlow]=arr[nhigh]
+                nlow+=1
+                break
+            else:
+                nhigh-=1
+        while(nlow<nhigh):
+            if(arr[nlow]>temp):
+                arr[nhigh]=arr[nlow]
+                nhigh-=1
+                break
+            else:
+                nlow+=1
+
+    arr[nlow]=temp
+    return nlow
+
+def Quick_sort(la,nlow,nhigh):
+    if(nlow<nhigh):
+        nstandard=f_sort(la,nlow,nhigh)
+        Quick_sort(la,nlow,nstandard-1)
+        Quick_sort(la,nstandard+1,nhigh)
 
 
 
@@ -43,12 +80,14 @@ def Print(arr):
         print(temp)
 
 def test(arr):
-    Bubble_sort(arr);
-    Select_sort(arr);
-    Print(arr);
+    Bubble_sort(arr)
+    Select_sort(arr)
+    Insert_sort(arr)
+    Quick_sort(arr,0,len(arr)-1)
+    Print(arr)
 
 if __name__=='__main__':
 
-    array=[23,12,32,56,21,1,5,9];
-    test(array);
+    array=[23,12,32,56,21,1,5,9]
+    test(array)
 
